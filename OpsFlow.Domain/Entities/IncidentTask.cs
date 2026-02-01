d1324ca221d37aa6705693564485f264e997e0ff
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-02-01 10:07:24 +0300

    feature: enum states
    states enum added for tracking incident tasks state.

--- OpsFlow.Domain/Entities/IncidentTask.cs
+++ OpsFlow.Domain/Entities/IncidentTask.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using OpsFlow.Domain.Enums;
 
 namespace OpsFlow.Domain.Entities
 {
@@ -8,5 +9,6 @@ namespace OpsFlow.Domain.Entities
         public string Name {get; set;}
         [Required]
         public Incident Incident {get; set;}
+        public States State {get; set;}
     }
 }
---
+++ OpsFlow.Domain/Enums/States.cs
@@ -0,0 +1,10 @@
+namespace OpsFlow.Domain.Enums
+{
+    public enum States
+    {
+        Open = 1,
+        Closed = 2,
+        Aborted = 3,
+        Waiting = 4
+    }
+}
