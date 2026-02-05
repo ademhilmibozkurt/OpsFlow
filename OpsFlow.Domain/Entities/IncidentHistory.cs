9215edeff4340d73db1128f998e17c575869dac4
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-02-05 15:07:05 +0300

    feature: incident history controls added

--- OpsFlow.Domain/Entities/IncidentHistory.cs
+++ OpsFlow.Domain/Entities/IncidentHistory.cs
@@ -5,17 +5,42 @@ namespace OpsFlow.Domain.Entities
         private int _incidentId;
         private Enum _eventType;
         private DateTime _occuredAt;
-        private int? _relatedTaskId;
         private int _performedById;
+        private int? _relatedTaskId;
 
-        public IncidentHistory(int incidentId, Enum eventType, DateTime occuredAt, int? relatedTaskId, int performedById)
+        public IncidentHistory(int incidentId, Enum eventType, DateTime occuredAt, int performedById, int? relatedTaskId)
         {
             // every event is splitted.
+            EnsureIdPositive(incidentId, "incidentId");
+            EnsureIdPositive(performedById, "performedById");
+
             _incidentId = incidentId;
             _eventType  = eventType;
             _occuredAt  = occuredAt;
-            _relatedTaskId = relatedTaskId;
             _performedById = performedById;
+
+            EnsureRelatedTaskId(relatedTaskId);
+            _relatedTaskId = relatedTaskId;
+        }
+
+        private void EnsureIdPositive(int id, string name)
+        {
+            if (id < 0)
+            {
+                throw new ArgumentException($"-{name}- can not be negative numbers!");
+            }
+        }
+
+        private void EnsureRelatedTaskId(int? id)
+        {
+            if (id == null)
+            {
+                return;
+            }
+            if (id < 0)
+            {
+                throw new ArgumentException("-relatedTaskId- can not be negative numbers!");
+            }
         }
     }
 }
