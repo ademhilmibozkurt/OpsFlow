b68277c5b7ac043ab7ece7cb6439e1517f2c4de1
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-02-06 08:16:43 +0300

    feature: application layer
    application layer structure done. folder and files created.

--- OpsFlow.Domain/Entities/BaseEntity.cs
+++ OpsFlow.Domain/Entities/BaseEntity.cs
@@ -1,10 +1,7 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace OpsFlow.Domain.Entities
 {
     public abstract class BaseEntity
     {
-        [Required][Key]
         public int Id {get; set;}
         public DateTime CreatedAt {get; set;}
         public DateTime UpdatedAt {get; set;}
