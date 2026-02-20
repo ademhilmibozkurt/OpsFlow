8924e5aaecf8c9e72720fbb50f1ab495069e4508
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-02-20 15:09:26 +0300

    feature: Add Identity
    Identity options added to program cs.

--- OpsFlow.Infrastructure/Program.cs
+++ OpsFlow.Infrastructure/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using OpsFlow.Application.Abstractions.Persistence;
 using OpsFlow.Infrastructure.Identity;
@@ -9,13 +10,35 @@ var builder = WebApplication.CreateBuilder(args);
 // add DbContext to services
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString(""));
 });
-
-builder.Services.AddIdentity<AppUser, AppRole>().AddEntityFrameworkStores<AppDbContext>();
 
 // dependency injection
 builder.Services.AddScoped<IIncidentRepository, IncidentRepository>();
+
+// add Identity
+builder.Services.AddIdentity<AppUser, AppRole>(options =>
+{
+    // user
+    options.User.RequireUniqueEmail = true;
+
+    // sign in options
+    options.SignIn.RequireConfirmedAccount = true;
+
+    // add password options
+    options.Password.RequireDigit = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 8;
+    options.Password.RequiredUniqueChars = 1;
+
+    // add lockout options
+    options.Lockout.MaxFailedAccessAttempts = 3;
+    options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+    options.Lockout.AllowedForNewUsers = true;
+})
+.AddEntityFrameworkStores<AppDbContext>();
 
 var app = builder.Build();
 app.UseAuthentication();
