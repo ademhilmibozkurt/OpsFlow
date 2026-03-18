bbcb7dba1a93b783ca571ceaf1a25d45a3f50af9
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-03-18 17:44:26 +0300

    delete: Incident Timeline

--- OpsFlow.Application/Incidents/Queries/GetIncidentTimeline/GetIncidentTimelineQuery.cs
+++
@@ -1,7 +0,0 @@
-using MediatR;
-using OpsFlow.Application.Incidents.Dtos;
-
-namespace OpsFlow.Application.Incidents.Queries.GetIncidentTimeline
-{
-    public sealed record GetIncidentTimelineQuery(string incidentId) : IRequest<IncidentTimelineResponseDto>;
-}
