0196650546514a5380699168f3079d8f3de1112a
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-04-05 11:16:58 +0300

    feature: Get Incidents Validator
    Validator logic added. Rules added for state and priority parameters.

---
+++ OpsFlow.Application/Incidents/Queries/GetIncidents/GetIncidentsValidator.cs
@@ -0,0 +1,27 @@
+using FluentValidation;
+using OpsFlow.Domain.Enums;
+
+namespace OpsFlow.Application.Incidents.Queries.GetIncidents
+{
+    public class GetIncidentsValidator : AbstractValidator<GetIncidentsQuery>
+    {
+        public GetIncidentsValidator()
+        {
+            RuleFor(v => v.State)
+                .NotEmpty()
+                .WithMessage("State can not null!");
+
+            RuleFor(v => v.State)
+                .Must(d => d is IncidentState)
+                .WithMessage("State must be a IncidentState enum type!");
+
+            RuleFor(v => v.Priority)
+                .NotEmpty()
+                .WithMessage("Priority can not null!");
+
+            RuleFor(v => v.Priority)
+                .Must(d => d is IncidentPriority)
+                .WithMessage("Priority must be IncidentPriority enum type!");
+        }
+    }
+}
