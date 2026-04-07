1f464e13a8f6fc275a02cb3f234cb0401176866e
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-04-07 12:32:53 +0300

    feature: Change Password Validator
    Validator logic added. rules added for current password and new password.new password checked with regex.

---
+++ OpsFlow.Application/Users/Commands/ChangePassword/ChangePasswordValidator.cs
@@ -0,0 +1,30 @@
+using FluentValidation;
+
+namespace OpsFlow.Application.Users.Commands.ChangePassword
+{
+    public class ChangePasswordValidator : AbstractValidator<ChangePasswordCommand>
+    {
+        public ChangePasswordValidator()
+        {
+            RuleFor(v => v.currentPassword)
+                .NotEmpty()
+                .WithMessage("CurrentPassword can not null!");
+
+            RuleFor(v => v.currentPassword)
+                .Must(d => d is string)
+                .WithMessage("CurrentPassword must be string!");
+
+            RuleFor(v => v.newPassword)
+                .NotEmpty()
+                .WithMessage("NewPassword can not null!");    
+
+            RuleFor(v => v.newPassword)
+                .Must(d => d is string)
+                .WithMessage("NewPassword must be string!");    
+
+            RuleFor(v => v.newPassword)
+                .Matches("(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}")
+                .WithMessage("NewPassword must be valid format!");
+        }
+    }
+}
