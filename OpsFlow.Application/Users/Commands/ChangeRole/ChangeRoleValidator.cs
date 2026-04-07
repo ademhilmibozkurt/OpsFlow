bb133d90a6be7488d6761140b8bf94e64b97e09e
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-04-07 12:35:31 +0300

    feature: Change Role Validator
    Validator logic added. rules added for user id and user role..

---
+++ OpsFlow.Application/Users/Commands/ChangeRole/ChangeRoleValidator.cs
@@ -0,0 +1,27 @@
+using FluentValidation;
+using OpsFlow.Application.Identity;
+
+namespace OpsFlow.Application.Users.Commands.ChangeRole
+{
+    public class ChangeRoleValidator : AbstractValidator<ChangeRoleCommand>
+    {
+        public ChangeRoleValidator()
+        {
+            RuleFor(v => v.userId)
+                .NotEmpty()
+                .WithMessage("UserId can not null!");
+
+            RuleFor(v => v.userId)
+                .Must(d => d is string)
+                .WithMessage("UserId must be string!");
+
+            RuleFor(v => v.userRole)
+                .NotEmpty()
+                .WithMessage("UserRole can not null!");    
+
+            RuleFor(v => v.userRole)
+                .Must(d => d is AppRole)
+                .WithMessage("UserRole must be AppRole type!");
+        }
+    }
+}
