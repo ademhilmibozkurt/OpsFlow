cec3bcc012ebc58752c8e5e3245bb8e7c88e6ee1
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-04-14 18:28:22 +0300

    fix: Create Task CH Null Note
    fixed with default value from command.

--- OpsFlow.Application/Tasks/Commands/CreateTask/CreateTaskCommand.cs
+++ OpsFlow.Application/Tasks/Commands/CreateTask/CreateTaskCommand.cs
@@ -3,5 +3,5 @@ using OpsFlow.Application.Tasks.Dtos;
 
 namespace OpsFlow.Application.Tasks.Commands.CreateTask
 {
-    public record CreateTaskCommand(string incidentId, string title, string? note) : IRequest<CreateTaskResponseDto>;
+    public record CreateTaskCommand(string incidentId, string title, string note = "") : IRequest<CreateTaskResponseDto>;
 }
