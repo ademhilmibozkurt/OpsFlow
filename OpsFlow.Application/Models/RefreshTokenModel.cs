bdc86a84d46d81cc61626f7028b5285225874200
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-04-14 18:17:35 +0300

    fix: Refresh Token Model Properties Nullable
    fixed with required keyword.

--- OpsFlow.Application/Models/RefreshTokenModel.cs
+++ OpsFlow.Application/Models/RefreshTokenModel.cs
@@ -2,8 +2,8 @@ namespace OpsFlow.Application.Models
 {
     public record RefreshTokenModel
     {
-        public string Token {get; set;}
-        public string UserId {get; set;}
+        public required string Token {get; set;}
+        public required string UserId {get; set;}
         public DateTime ExpiresAt {get; set;}
         public bool IsRevoked {get; set;} = false;
     }
