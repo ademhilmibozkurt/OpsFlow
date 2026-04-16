88f1ba2c585c5438bb7f8284b1b8112e925192fd
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-04-16 13:02:32 +0300

    fix: Smtp Settings Null Fields
    fixed with making required.

--- OpsFlow.Infrastructure/Settings/SmtpSettings.cs
+++ OpsFlow.Infrastructure/Settings/SmtpSettings.cs
@@ -2,10 +2,10 @@ namespace OpsFlow.Infrastructure.Settings
 {
     public sealed class SmtpSettings
     {
-        public string Host { get; init; }
+        public required string Host { get; init; }
         public int Port { get; init; }
-        public string UserName { get; init; }
-        public string Password { get; init; }
-        public string From { get; init; }
+        public required string UserName { get; init; }
+        public required string Password { get; init; }
+        public required string From { get; init; }
     }
 }
