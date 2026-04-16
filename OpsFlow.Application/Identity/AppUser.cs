c250e1d9179075c4cfe8c7c0491c6f31c4acb41d
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-04-16 12:23:49 +0300

    refactor: AppUser Fields Override
    Fields overrided for supressing IdentityUser fields possible null warnings

--- OpsFlow.Application/Identity/AppUser.cs
+++ OpsFlow.Application/Identity/AppUser.cs
@@ -5,6 +5,9 @@ namespace OpsFlow.Application.Identity
     public class AppUser : IdentityUser
     {
         public required string FullName {get; set;}
+        public new required string UserName {get; set;}
+        public new required string Email {get; set;}
+        public new required string PhoneNumber {get; set;}
         public DateTime CreatedAt {get; set;}
         public AppRole Role {get; set;}
     }
