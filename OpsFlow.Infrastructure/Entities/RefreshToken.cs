abd50ee20d2bebc5b5900fb71acb15bcc1ba6dff
ademhilmibozkurt <bozkurtadem7@gmail.com>
2026-04-17 15:40:25 +0300

    fix: Refresh Token Possible Null Fields
    fixed with making them required.

--- OpsFlow.Infrastructure/Entities/RefreshToken.cs
+++ OpsFlow.Infrastructure/Entities/RefreshToken.cs
@@ -3,8 +3,8 @@ namespace OpsFlow.Infrastructure.Entities
     public class RefreshToken
     {
         public Guid Id { get; set; }
-        public string Token { get; set; }
-        public string UserId { get; set; }
+        public required string Token { get; set; }
+        public required string UserId { get; set; }
         public DateTime ExpiresAt { get; set; }
         public bool IsRevoked { get; set; }
     }
